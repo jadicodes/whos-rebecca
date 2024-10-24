a990af350c644f98b968824ee88ff24f6d7635f1
Jadi Miller <jadilmiller@gmail.com>
2024-10-24 15:58:28 -0400

    Add scrolling Rebeccas to the main menu

--- project/game/game.gd
+++ project/game/game.gd
@@ -6,6 +6,8 @@ signal answer_locked_in
 @onready var _portrait_grid = %PortraitGridContainer
 
 
+# Choose 4 random Rebecca's from the array of all Rebecca's
+
 func _choose_random_characters(character: String):
 	var picked_characters: Array[CharacterBase]
 	var pool_of_characters = _rebeccas
@@ -20,6 +22,8 @@ func _choose_random_characters(character: String):
 	_set_traits(correct_rebecca)
 
 
+# Iterate through children of PortraitGridContainer and set texture property to portrait
+
 func _create_portraits(array_of_characters):
 	var counter = 0
 	for child in _portrait_grid.get_children():
@@ -28,11 +32,15 @@ func _create_portraits(array_of_characters):
 	counter = 0
 
 
+# Set the three traits of the selected Rebecca in the textbox
+
 func _set_traits(rebecca):
 	rebecca.traits.shuffle()
 	print(rebecca.traits[0], rebecca.traits[1], rebecca.traits[2])
 	$Textbox.set_traits(rebecca.traits[0], rebecca.traits[1], rebecca.traits[2])
 
+
+# When lock in button is pressed, emit signal to alert main
 
 func _on_lock_in_button_pressed() -> void:
 	emit_signal("answer_locked_in")
--- project/main_menu/main_menu.gd
+++ project/main_menu/main_menu.gd
@@ -5,3 +5,11 @@ signal game_started
 
 func _on_start_button_pressed() -> void:
 	emit_signal("game_started")
+
+
+func _on_explanation_button_pressed() -> void:
+	$ExplanationPanel.show()
+
+
+func _on_ok_button_pressed() -> void:
+	$ExplanationPanel.hide()
