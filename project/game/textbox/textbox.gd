fd39c00e53afb7221bc8d4732b3cfcfc50677556
Jadi Miller <jadilmiller@gmail.com>
2024-10-24 12:52:20 -0400

    Format traits in a notebook

--- project/game/textbox/textbox.gd
+++ project/game/textbox/textbox.gd
@@ -2,6 +2,6 @@ extends Control
 
 
 func set_traits(trait1, trait2, trait3) -> void:
-	%Trait1.text = str(trait1)
-	%Trait2.text = str(trait2)
-	%Trait3.text = str(trait3)
+	%Trait1.text = "* " + str(trait1)
+	%Trait2.text = "* " + str(trait2)
+	%Trait3.text = "* " + str(trait3)
